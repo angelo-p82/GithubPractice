119a0c5a5c06cf92e3659db335b57d86002b7764
Angelo Pallotta <angelo.p82@gmail.com>
2023-01-19 11:09:37 +0100

    Message color changed to yellow

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.ForegroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("This is the second message.");
 Console.WriteLine("Press any key to continue... Bye");
 Console.ReadLine();
