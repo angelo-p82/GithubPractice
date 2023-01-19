39a0565de8a731e07706dfbee3a6d3a4cf8f813a
Angelo Pallotta <angelo.p82@gmail.com>
2023-01-19 18:13:00 +0100

    rebase commit 1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,4 @@ Console.WriteLine("This is the second message.");
 Console.WriteLine("Press any key to continue... Bye");
 Console.ReadLine();
 
-// Master commit 1
-// Master commit 2
-// Master commit 3
+// rebase commit 1
