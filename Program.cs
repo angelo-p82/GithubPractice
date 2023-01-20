6795dfea2a1658f3b235b18094a2ba9cf247820e
Angelo Pallotta <angelo.p82@hotmail.com>
2023-01-20 09:45:22 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
 // See https://aka.ms/new-console-template for more information
-// modified
 Console.WriteLine("Hello, World!");
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("This is the second message.");
 Console.WriteLine("Press any key to continue... Bye");
 Console.ReadLine();
-
-//master commit 1
-//master commit 1
-//rebase commit 1
-//rebase commit 2
